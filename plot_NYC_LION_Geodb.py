864f4d5401e5e5d2d9a042f43463ebba4ed8fde1
Jose E. Nunez <je.nunez@yahoo.ca>
2015-07-27 20:13:44 -0400

    generic area_thresh= in matplotlib/basemap to visualize Governors Island, Hart Island, etc, as LION GeoDB does
    The New York City's Department of City Planning LION Single Line Street Base Map has segments for many
    streets, as well as for those in these islands, so this generic tries to match the basemap with the
    LION GeoDB.

--- plot_NYC_LION_Geodb.py
+++ plot_NYC_LION_Geodb.py
@@ -19,12 +19,26 @@ LOW_LEFT_CORNER_LATITUDE = 40.485808
 UP_RIGHT_CORNER_LONGITUDE = -73.699206
 UP_RIGHT_CORNER_LATITUDE = 40.917691
 
+# This parameter refers to the minimum island in New York City to visualize
+# in the matplotlib/basemap, e.g, Governors Island or Hart Island. The New
+# York City's Department of City Planning LION Single Line Street Base Map
+# has segments for many streets, as well as for those in these islands, so
+# they appear plotted by default by the readshapefile('LION_geodb') below
+# and this parameter below controls the minimum coastline to plot with
+# Basemap(). With 0.9, Governors Island is visualized in the Basemap, with
+# 0.6, Hart Island does.
+
+MIN_NYC_ISLAND_TO_VISUALIZ = 0.6
+
+# Create the Basemap
+
 m = Basemap(llcrnrlon=LOW_LEFT_CORNR_LONGITUDE,
             llcrnrlat=LOW_LEFT_CORNER_LATITUDE,
             urcrnrlon=UP_RIGHT_CORNER_LONGITUDE,
             urcrnrlat=UP_RIGHT_CORNER_LATITUDE,
             ellps='WGS84',
-            resolution='h', area_thresh=1)
+            resolution='h',
+            area_thresh=MIN_NYC_ISLAND_TO_VISUALIZ)
 
 m.drawcoastlines()
 m.fillcontinents(color='green')
